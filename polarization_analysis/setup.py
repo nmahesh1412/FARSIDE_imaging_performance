878bb14004f4b53c7aff67219a3288c0f12faf1f
nmahesh1412 <nivedita.mahesh@asu.edu>
2021-12-27 16:00:33 -0700

    making the image calculation functions a package and importable

---
+++ polarization_analysis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='image_code',
+version='0.1',
+description='image calculation functions',
+url='#',
+author='NMahesh',
+author_email='nmahesh4@asu.edu',
+license='ASU',
+packages=['image_code'],
+zip_safe=False)
